60e7f9c0bb2863020aa70acd6bb9700dcb79db7b
Atsushi Koike <koike@shiga777.com>
2016-08-29 21:56:01 +0000

    課題２進行中

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160824154031) do
+ActiveRecord::Schema.define(version: 20160829214956) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
