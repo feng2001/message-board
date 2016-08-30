5d476d7d05999f79001a8a5dc804f93902c4a011
Atsushi Koike <koike@shiga777.com>
2016-08-30 14:55:52 +0000

    BootStrapを修正（あとエラーの返しも微修正）

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,5 @@ class Message < ActiveRecord::Base
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
   #年齢は０歳以上 #
-   validates :age , inclusion: { in: 0..150 }
+   validates :age , numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 150 } , presence: true
 end
